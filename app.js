8733e1ff032ba7154abc25f5c74d8b0caa54ca25
Kellen Presta <kpresta04@gmail.com>
2020-02-16 13:48:32 -0600

    get name added

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const inquirer = require("inquirer");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+
+async function main() {
+  async function getName() {
+    let nameQuery = await inquirer.prompt({
+      message: "What is your name?",
+      name: "name"
+    });
+    // nameQuery = await nameQuery;
+    return nameQuery.name;
+  }
+  const name = await getName();
+
+  console.log(typeof name);
+}
+main();
