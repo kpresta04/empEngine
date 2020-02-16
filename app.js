e5529b4f7e4f1d6f17451636084c8cd9d352bd31
Kellen Presta <kpresta04@gmail.com>
2020-02-16 16:02:44 -0600

    trying

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
+const fs = require("fs");
 
 async function main() {
   async function getRole() {
@@ -21,11 +22,16 @@ async function main() {
     return uq.result;
   }
 
-  async function getCardInfo() {
+  async function makeObj() {
     if (role === "Engineer") {
       const github = await userQuery("What is your github username?");
-      const eng = new Engineer(empName, id, email, github);
-      console.log(eng);
+      return new Engineer(empName, id, email, github);
+    } else if (role === "Manager") {
+      const officeNum = await userQuery("What is your office number?");
+      return new Manager(empName, id, email, officeNum);
+    } else {
+      const schoolName = await userQuery("What is the name of your school?");
+      return new Intern(empName, id, email, schoolName);
     }
   }
 
@@ -34,6 +40,113 @@ async function main() {
   const email = await userQuery("What is your email?");
   const id = await userQuery("What is your company ID?");
 
-  getCardInfo();
+  let newObj = await makeObj();
+  console.log(newObj);
+
+  let cardHTML = `<div class="card my-1 mx-2" style="width: 18rem;">
+  <div
+    class="card-body"
+    style="background-color: rgb(250, 234, 234);"
+  >
+    <div class="titleRow">
+     <h2 class="card-title">
+    ${newObj[0]}
+  </h2>
+  <h2>${newObj.getRole()}</h2>
+  </div>
+  
+  <p class="card-text my-2">
+  ID: ${newObj[1]}
+</p>
+<hr />
+<p>Email: ${newObj[2]}</p>
+<hr />
+<p>Github: ${newObj[3]}</p>
+  </div>
+  </div>`;
+
+  let pageHTML = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous"
+    />
+
+    <title>My Team</title>
+    <style>
+      body,
+      html {
+        height: 100%;
+      }
+      #headerRow {
+        color: white;
+        background-color: rgb(184, 52, 52);
+        height: 8em;
+      }
+      #headerRow h1 {
+        padding-top: 1em;
+      }
+      .col {
+        display: flex;
+        margin: 4em;
+      }
+      .titleRow {
+        color: white;
+        background-color: #3d7bff;
+        padding: 1em;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container-fluid text-center">
+      <div class=" text-center" id="headerRow">
+        <h1 class="text-center">My Team</h1>
+      </div>
+      <div class="row">
+        <div class="col justify-content-center" id="cardCol">${cardHTML}
+          
+
+        
+          
+          <!-- card col ends -->
+        </div>
+      </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script
+      src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
+`;
+
+  fs.writeFile("output.html", pageHTML, "utf8", err => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
 }
 main();
---
+++ tester.js
@@ -0,0 +1,22 @@
+let cardHTML = [
+  `<div class="card" style="width: 18rem;">
+<div
+  class="card-body"
+  style="background-color: rgb(250, 234, 234);"
+>
+  <div class="titleRow">`,
+  `  <h2 class="card-title">
+  My Name
+</h2>
+<h2>My Role</h2>
+</div>
+`,
+  `  <p class="card-text my-2">
+Some quick example text to build on the card title and make up
+the bulk of the card's content.
+</p>
+</div>
+</div>`
+];
+
+console.log(array1.join(""));
